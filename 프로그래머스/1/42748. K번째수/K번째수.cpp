5cecdd85e442d837dc210ad55fbbd75c3dcf5297
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-06-27 13:43:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -8,13 +8,16 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     for (int i = 0; i < commands.size(); ++i)
     {
         std::vector<int> Vec;
-        for (int j = commands[i][0] - 1; j <= commands[i][1] - 1; ++j)
+        int Start = commands[i][0] - 1;
+        int End = commands[i][1] - 1;
+        int K = commands[i][2] - 1;
+        for (int j = Start; j <= End; ++j)
         {   
             Vec.push_back(array[j]);
         }
         
         std::sort(Vec.begin(), Vec.end());
-        answer.push_back(Vec[commands[i][2] - 1]);
+        answer.push_back(Vec[K]);
     }
 
     return answer;
