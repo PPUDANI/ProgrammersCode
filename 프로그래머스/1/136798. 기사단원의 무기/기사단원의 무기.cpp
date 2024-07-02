4c5fd31c3630f31f890419fd35f1d43816a535a4
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-07-02 14:29:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.40 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -5,47 +5,57 @@ using namespace std;
 
 int solution(int number, int limit, int power) {
     int answer = 0;
-    
-    for (int CurNum = 1; CurNum <= number; ++CurNum) // 모든 기사 순회
-    {
-        int DivNum = CurNum; // 나눠지는 수
-        int CurDiv = 2; // 나누는 수
-        int DivCount = 0; // CurDiv로 나눠진 횟수
-        int Divisor = 1; // 약수 결과값
+ static int PrevNumber = 0;
 
-        // 소인수분해
-        while(1)
-        {
-            if (DivNum % CurDiv == 0)
-            {
-                ++DivCount;
-                DivNum /= CurDiv;
-            }
-            else
-            {
-                if (DivCount != 0) // CurDiv로 나눠진 경우
-                {
-                    Divisor *= (1 + DivCount);
-                    DivCount = 0;
-                }
+ static std::vector<int> DivisorVec;
 
-                if (CurDiv > DivNum) // CurDiv가 DivNum보다 큰 경우
-                {
-                    break;
-                }
+ for (int CurNum = PrevNumber + 1; CurNum <= number; ++CurNum) // 모든 기사 순회
+ {
+     int DivNum = CurNum; // 나눠지는 수
+     int CurDiv = 2; // 나누는 수
+     int DivCount = 0; // CurDiv로 나눠진 횟수
+     int Divisor = 1; // 약수 결과값
 
-                ++CurDiv;
-            }
-        }
+     // 소인수분해
+     while(1)
+     {
+         if (DivNum % CurDiv == 0)
+         {
+             ++DivCount;
+             DivNum /= CurDiv;
+         }
+         else
+         {
+             if (DivCount != 0) // CurDiv로 나눠진 경우
+             {
+                 Divisor *= (1 + DivCount);
+                 DivCount = 0;
+             }
 
-        if (Divisor > limit)
-        {
-            answer += power;
-        }
-        else
-        {
-            answer += Divisor;
-        }
-    }
-    return answer;
+             if (CurDiv > DivNum) // CurDiv가 DivNum보다 큰 경우
+             {
+                 DivisorVec.push_back(Divisor);
+                 break;
+             }
+
+             ++CurDiv;
+         }
+     }
+
+     
+ }
+
+ for (int i = 0; i < number; ++i)
+ {
+     if (DivisorVec[i] > limit)
+     {
+         answer += power;
+     }
+     else
+     {
+         answer += DivisorVec[i];
+     }
+ }
+ PrevNumber = number;
+ return answer;
 }
