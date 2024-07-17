25b4fbb947809248445fb0b568c6c58213242201
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-07-17 17:06:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -5,25 +5,26 @@ using namespace std;
 
 int solution(int num) {
     int answer = 0;
-    long long DNum = num;
-    while(DNum != 1)
+    long long DNum = static_cast<long long>(num);
+
+    while (DNum != 1)
     {
-        if(DNum % 2 == 0)
+        if (DNum % 2 == 0)
         {
             DNum /= 2;
+            ++answer;
         }
         else
         {
-            DNum = (DNum * 3) + 1;
+            DNum = ((DNum * 3) + 1) / 2;
+            answer += 2;
         }
-        
-        ++answer;
     }
-    
-    if(answer >= 500)
+
+    if (answer >= 500)
     {
         answer = -1;
     }
-    
+
     return answer;
 }
