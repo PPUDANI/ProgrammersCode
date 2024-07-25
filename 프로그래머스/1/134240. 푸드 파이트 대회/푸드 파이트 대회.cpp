643dfa8e44352d22f17d4d1ff0665ffbebbeb1fd
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-07-26 00:06:09 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -2,13 +2,12 @@
 #include <vector>
 
 using namespace std;
-
 string solution(vector<int> food)
 {
     std::string answer = "";
-    
+
     int NumberOfFoodTypes = static_cast<int>(food.size());
-    
+
     std::vector<std::string> Foods(NumberOfFoodTypes, "");
 
 
@@ -16,10 +15,6 @@ string solution(vector<int> food)
     {
         int NumberOfFood = food[i] / 2;
         Foods[i].assign(NumberOfFood, i + '0');
-    }
-
-    for (int i = 0; i < NumberOfFoodTypes; ++i)
-    {
         answer += Foods[i];
     }
 
