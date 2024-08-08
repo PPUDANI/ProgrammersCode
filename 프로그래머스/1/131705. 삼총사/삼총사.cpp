d95fd4ffdec6225320550af25ef2c45816c73a04
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-08-08 13:21:48 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int VectorNumber = static_cast<int>(number.size());
+    for (int i = 0; i < VectorNumber - 2; ++i)
+    {
+        for (int j = i + 1; j < VectorNumber - 1; ++j)
+        {
+            for (int k = j + 1; k < VectorNumber; ++k)
+            {
+                if (number[i] + number[j] + number[k] == 0)
+                {
+                    ++answer;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
