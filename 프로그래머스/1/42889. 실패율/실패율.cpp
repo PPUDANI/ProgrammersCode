844429133741a7c40dd96a4bed698e5ffebb30d2
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-08-14 17:15:27 +0900

    [level 1] Title: 실패율, Time: 1.27 ms, Memory: 9.88 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <cmath>    
+#include <algorithm>
+
+bool compare(std::pair<float, int> Left, std::pair<float, int> Right)
+{
+    if (Left.first == Right.first)
+    {
+        return Left.second < Right.second;
+    }
+
+    return Left.first > Right.first;
+}
+
+std::vector<int> solution(int N, std::vector<int> stages)
+{
+    std::vector<std::pair<float, int>> StageClearCounts;
+
+    int PlayerNumber = static_cast<int>(stages.size());
+
+    for (int i = 1; i < N + 2; ++i)
+    {
+        StageClearCounts.push_back(std::make_pair(0.0f, i));
+    }
+
+    for (int i : stages)
+    {
+        ++StageClearCounts[i - 1].first;
+    }
+
+    std::transform(StageClearCounts.begin(), StageClearCounts.end(), StageClearCounts.begin(), [&](std::pair<float, int> i)
+        {
+            if (PlayerNumber <= 0)
+            {
+                i.first = 0;
+                return i;
+            }
+
+            int Num =  static_cast<int>(i.first);
+            i.first /= static_cast<float>(PlayerNumber);
+            PlayerNumber -= Num;
+            return i;
+        });
+
+    std::sort(StageClearCounts.begin(), StageClearCounts.end() - 1, compare);
+    std::vector<int> answer;
+
+    for (int i = 0; i < N; ++i)
+    {
+        answer.push_back(StageClearCounts[i].second);
+    }
+
+    return answer;
+}
