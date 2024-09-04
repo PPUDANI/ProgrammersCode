6a26a1d30655908fce34e026878b1f42b2709f97
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-09-04 23:10:31 +0900

    [level 1] Title: 햄버거 만들기, Time: 14.45 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> ingredient)
+{
+    int answer = 0;
+    int NumberOfIngredient = static_cast<int>(ingredient.size());
+
+    stack<int> Burger;
+    Burger.push(-1);
+
+    for (int i = 0; i < NumberOfIngredient; ++i)
+    {
+        if (ingredient[i] == 1)
+        {
+            if (Burger.top() == 123)
+            {
+                Burger.pop();
+                ++answer;
+                continue;
+            }
+
+            Burger.push(1);
+            continue;
+        }
+
+        if (ingredient[i] == 2 && Burger.top() == 1)
+        {
+            Burger.top() = 12;
+            continue;
+        }
+
+        if (ingredient[i] == 3 && Burger.top() == 12)
+        {
+            Burger.top() = 123;
+            continue;
+        }
+
+        Burger.push(ingredient[i]);
+    }
+    return answer;
+}
