021920e0d5d8d5f2d49ccee88c3e993af3fad12c
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-09-23 13:50:16 +0900

    [level 2] Title: n^2 배열 자르기, Time: 45.33 ms, Memory: 23.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    answer.reserve(right - left);
+
+    int LeftIndexY = left / n;
+    int LeftIndexX = left % n;
+
+    int RightIndexY = right / n;
+    int RightIndexX = right % n;
+
+
+    for (int Y = LeftIndexY; Y <= RightIndexY; ++Y)
+    {
+        int Count = (Y == LeftIndexY) ? LeftIndexX : 0;
+        int Max_X = (Y == RightIndexY) ? RightIndexX + 1 : n;
+
+        for (int X = Count; X < Max_X; ++X)
+        {
+            int Eliment = Count < Y ? Y + 1 : X + 1;
+            answer.push_back(Eliment);
+            ++Count;
+        }
+    }
+
+    return answer;
+}
