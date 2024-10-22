a7a42ac2baa3be4be462755ff66326336025f8d3
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-10-22 21:51:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 12.41 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,54 +1,51 @@
-#include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> edge) 
 {
     vector<vector<int>> Graph(n + 1, vector<int>());
-
     for (const vector<int>& EdgeInfo : edge)
     {
         Graph[EdgeInfo[0]].push_back(EdgeInfo[1]);
         Graph[EdgeInfo[1]].push_back(EdgeInfo[0]);
     }
 
-    vector<int> DistanceToNode(n + 1, 0);
-    vector<bool> VisitedNode(n + 1, false);
-    int MaxDist = 0;
+    vector<int> Distances(n + 1, 0);
+    vector<bool> IsVisited(n + 1, false);
+    int MaxDistance = 0;
 
     queue<int> BFS_Q;
     BFS_Q.push(1);
-    VisitedNode[1] = true;
+    IsVisited[1] = true;
     while (!BFS_Q.empty())
     {
         int CurNode = BFS_Q.front();
         BFS_Q.pop();
 
-        int DistToNextNode = DistanceToNode[CurNode] + 1;
-
+        int CurDistance = Distances[CurNode] + 1;
         for (int NextNode : Graph[CurNode])
         {
-            if(VisitedNode[NextNode] == false)
+            if(IsVisited[NextNode] == false)
             {
-                DistanceToNode[NextNode] = DistToNextNode;
-
-                if (DistToNextNode > MaxDist)
+                Distances[NextNode] = CurDistance;
+                if (Distances[NextNode] > MaxDistance)
                 {
-                    MaxDist = DistToNextNode;
+                    MaxDistance = max(MaxDistance, CurDistance);
                 }
 
-                VisitedNode[NextNode] = true;
+                IsVisited[NextNode] = true;
                 BFS_Q.push(NextNode);
             }
         }
     }
 
     int answer = 0;
-    for (int Dist : DistanceToNode)
+    for (int Dist : Distances)
     {   
-        if (Dist == MaxDist)
+        if (Dist == MaxDistance)
         {
             ++answer;
         }
