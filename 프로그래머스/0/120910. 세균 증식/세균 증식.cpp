afe76bde43a2e00b52cca0f2b457a458e8461485
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-02 09:44:14 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -4,12 +4,8 @@
 using namespace std;
 
 int solution(int n, int t) {
-    int answer =  n;
+    int answer =  n * pow(2, t);
     
-    for(int i = 0; i < t; ++i)
-    {
-        answer = answer * 2;
-    }
    
     return answer;
 }
