ab4ed5d2d97d32af3eb31615740b13818c565c00
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-04 15:53:56 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) 
+{
+    sort(num_list.begin(), num_list.end(), [](int Left, int Right)
+         {
+             return Left < Right;
+         });
+
+    vector<int> answer(num_list.begin() + 5, num_list.end());
+    
+    return answer;
+}
