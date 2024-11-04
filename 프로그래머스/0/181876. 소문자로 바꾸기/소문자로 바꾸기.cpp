2ec1bb53573c55a403e572c98071c1b9acba6251
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-04 16:41:28 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(char c : myString)
+    {
+        answer += tolower(c);
+    }
+    
+    return answer;
+}
