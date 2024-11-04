aeac58363f9d3c770f82ff35dbca81d8271449ff
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-04 16:37:06 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(char c : myString)
+    {
+        if(c == 'a' || c == 'A')
+        {
+            c = 'A';
+        }
+        else if(isupper(c))
+        {
+            c = tolower(c);
+        }
+        
+        answer += c;
+    }
+    return answer;
+}
