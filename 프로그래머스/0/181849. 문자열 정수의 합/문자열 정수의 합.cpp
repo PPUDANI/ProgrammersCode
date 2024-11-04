8f6e83f11d0cc75952ff123a9b5bee5eb9d1666f
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-04 15:50:38 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cpp
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cpp
@@ -6,13 +6,11 @@ using namespace std;
 int solution(string num_str) {
     int answer = 0;
     
-    int Count = 0;
     for(const char c : num_str)
     {
         answer += c;
-        ++Count;
     }
-    answer -= '0' * Count;
+    answer -= int(num_str.length()) * '0';
     
     return answer;
 }
