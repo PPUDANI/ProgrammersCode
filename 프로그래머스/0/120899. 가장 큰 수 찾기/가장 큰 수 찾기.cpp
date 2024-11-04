d038465090624e023e0c99957dfb62fe116ead71
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-04 17:01:20 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -8,9 +8,10 @@ vector<int> solution(vector<int> array)
 {
     vector<int> answer;
     
-    int Max = *(max_element(array.begin(), array.end()));
-    answer.push_back(Max);
-       answer.push_back(max_element(array.begin(), array.end()) - array.begin()); 
+    vector<int>::iterator MaxIndex = max_element(array.begin(), array.end());
+    
+    answer.push_back(*MaxIndex);
+    answer.push_back(int(MaxIndex - array.begin())); 
     
     return answer;
 }
