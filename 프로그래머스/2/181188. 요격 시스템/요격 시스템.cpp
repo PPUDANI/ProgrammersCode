59a23c85d09e0b08e11208f851256e6c3de716f9
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-06 17:48:09 +0900

    [level 2] Title: 요격 시스템, Time: 232.03 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -3,78 +3,25 @@
 #include <algorithm>
 using namespace std;
 
-class int2
+bool comp(const vector<int>& Left, const vector<int>& Rigjt)
 {
-public:
-    int2(int _Start = 0, int _End = 0)
-        : Start(_Start), End(_End)
-    {
-
-    }
-    
-    static const int2 NotOverlap;
-
-    static int2 GetOverlapRange(int2 _Left, int2 _Right)
-    {
-        int2 Res;
-
-        Res.Start = _Left.Start > _Right.Start ? _Left.Start : _Right.Start;
-        Res.End = _Left.End < _Right.End ? _Left.End : _Right.End;
-
-        if (Res.Start >= Res.End)
-        {
-            return NotOverlap;
-        }
-
-        return Res;
-    }
-
-    bool operator == (int2 _Val)
-    {
-        bool Res = Start == _Val.Start && End == _Val.End;
-        return Res;
-    }
-
-    bool operator != (int2 _Val)
-    {
-        bool Res = Start != _Val.Start || End != _Val.End;
-        return Res;
-    }
-    int Start;
-    int End;
-};
-
-const int2 int2::NotOverlap = int2(0, 0);
-
+    return Left[1] < Rigjt[1];
+}
 
 int solution(vector<vector<int>> targets) 
 {
-    sort(targets.begin(), targets.end(), [](const vector<int>& Left, const vector<int>& Right)
-        {
-            return Left[0] < Right[0];
-        }
-    );
-
-    vector<int2> Shots;
-    Shots.push_back(int2(targets[0][0], targets[0][1]));
-
-    for (const vector<int>& Target : targets)
+    sort(targets.begin(), targets.end(), comp);
+        
+    int InterceptCnt = 0;
+    int InterceptCoord = -1;
+    for(vector<int> Target : targets)
     {
-        int2 OverlapRange;
-        OverlapRange = int2::GetOverlapRange(Shots.back(), int2(Target[0], Target[1]));
-
-        if (OverlapRange != int2::NotOverlap)
+        if(Target[0] >= InterceptCoord)
         {
-            Shots.back() = OverlapRange;
-            continue;
-        }
-
-        if (OverlapRange == int2::NotOverlap)
-        {
-            Shots.push_back(int2(Target[0], Target[1]));
+            InterceptCoord = Target[1];
+            ++InterceptCnt;
         }
     }
-
-    int answer = static_cast<int>(Shots.size());
-    return answer;
+    
+    return InterceptCnt;
 }
