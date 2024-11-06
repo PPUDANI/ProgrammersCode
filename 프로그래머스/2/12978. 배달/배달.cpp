6d3059c5c0f066d1a5332e41f682274061ed19b8
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-06 23:26:01 +0900

    [level 2] Title: 배달, Time: 0.41 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -4,62 +4,57 @@
 
 using namespace std;
 
-class Edge {
-public:
-    int Distnation;
-    int Cost;
+#define INF 999999
 
-    bool operator > (const Edge& _Value) const
-    {
-        return Cost > _Value.Cost;
-    }
-};
-
-int solution(int N, vector<vector<int>> road, int K) 
+int solution(int N, vector<vector<int> > road, int K)
 {
-    vector<vector<Edge>> Graph(N + 1);
-
-    for (const vector<int>& RoadInfo : road)
+    vector<vector<pair<int, int>>> Graph(N + 1);
+    for (vector<int> Edge : road)
     {
-        Graph[RoadInfo[0]].push_back({ RoadInfo[1], RoadInfo[2] });
-        Graph[RoadInfo[1]].push_back({ RoadInfo[0], RoadInfo[2] });
+        int From = Edge[0];
+        int To = Edge[1];
+        int Dist = Edge[2];
+        
+        Graph[From].push_back({Dist, To});
+        Graph[To].push_back({Dist, From});
     }
 
-    vector<int> CostToNode(N + 1, INT32_MAX);
-    priority_queue<Edge, vector<Edge>, greater<Edge>> BFS_PQ;
+    vector<int> DistToNodes(N + 1, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> Dijkstra_PQ;
+    
+    DistToNodes[1] = 0;
+    Dijkstra_PQ.push({ 0, 1 });
 
-    CostToNode[1] = 0;
-    BFS_PQ.push({ 1, 0 });
-
-    while (!BFS_PQ.empty())
+    while (!Dijkstra_PQ.empty())
     {
-        Edge CurEdge = BFS_PQ.top();
-        BFS_PQ.pop();
+        int CurNode = Dijkstra_PQ.top().second;
+        int CurDist = Dijkstra_PQ.top().first;
+        Dijkstra_PQ.pop();
 
-        if (CurEdge.Cost > CostToNode[CurEdge.Distnation])
+        if (DistToNodes[CurNode] < CurDist)
         {
             continue;
         }
 
-        for (Edge DistNode : Graph[CurEdge.Distnation])
+        for (auto Edge : Graph[CurNode])
         {
-            DistNode.Cost += CostToNode[CurEdge.Distnation];
+            int NextNode = Edge.second;
+            int NewDist = Edge.first + CurDist;
 
-            if (DistNode.Cost < CostToNode[DistNode.Distnation])
+            if (DistToNodes[NextNode] > NewDist)
             {
-                CostToNode[DistNode.Distnation] = DistNode.Cost;
-                BFS_PQ.push(DistNode);
+                DistToNodes[NextNode] = NewDist;
+                Dijkstra_PQ.push({ NewDist, NextNode });
             }
         }
     }
 
-    // K 이하로 배달 가능한 마을 수 카운트
     int answer = 1;
-    for (int i = 2; i <= N; ++i) 
+    for (int i = 2; i <= N; ++i)
     {
-        if (CostToNode[i] <= K)
+        if (K >= DistToNodes[i])
         {
-            answer++;
+            ++answer;
         }
     }
 
