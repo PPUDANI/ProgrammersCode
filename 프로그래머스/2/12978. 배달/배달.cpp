0e7e7a8bc7b8ab9bcd100c0817a90fa96c8ab044
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-06 23:43:06 +0900

    [level 2] Title: 배달, Time: 0.35 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,62 +1,60 @@
+#include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
-
 using namespace std;
 
 #define INF 999999
 
-int solution(int N, vector<vector<int> > road, int K)
+int solution(int N, vector<vector<int> > road, int K) 
 {
     vector<vector<pair<int, int>>> Graph(N + 1);
-    for (vector<int> Edge : road)
+    for(auto Edge : road)
     {
         int From = Edge[0];
         int To = Edge[1];
-        int Dist = Edge[2];
+        int Cost = Edge[2];
         
-        Graph[From].push_back({Dist, To});
-        Graph[To].push_back({Dist, From});
+        Graph[From].push_back({Cost, To});
+        Graph[To].push_back({Cost, From});
     }
-
-    vector<int> DistToNodes(N + 1, INF);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> Dijkstra_PQ;
     
-    DistToNodes[1] = 0;
-    Dijkstra_PQ.push({ 0, 1 });
-
-    while (!Dijkstra_PQ.empty())
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ;
+    vector<int> CostToNode(N + 1, INF);
+    
+    PQ.push({0, 1});
+    CostToNode[1] = 0;
+    
+    while(!PQ.empty())
     {
-        int CurNode = Dijkstra_PQ.top().second;
-        int CurDist = Dijkstra_PQ.top().first;
-        Dijkstra_PQ.pop();
-
-        if (DistToNodes[CurNode] < CurDist)
+        int CurCost = PQ.top().first;
+        int CurNode = PQ.top().second;
+        
+        PQ.pop();
+        
+        if(CurCost > CostToNode[CurNode])
         {
             continue;
         }
-
-        for (auto Edge : Graph[CurNode])
+        
+        for(auto Edge : Graph[CurNode])
         {
+            int NewCost = Edge.first + CurCost;
             int NextNode = Edge.second;
-            int NewDist = Edge.first + CurDist;
-
-            if (DistToNodes[NextNode] > NewDist)
+            if(CostToNode[NextNode] > NewCost)
             {
-                DistToNodes[NextNode] = NewDist;
-                Dijkstra_PQ.push({ NewDist, NextNode });
+                CostToNode[NextNode] = NewCost;
+                PQ.push({NewCost, NextNode});
             }
         }
     }
-
+    
     int answer = 1;
-    for (int i = 2; i <= N; ++i)
+    for(int i = 2; i <= N; ++i)
     {
-        if (K >= DistToNodes[i])
+        if(CostToNode[i] <= K)
         {
             ++answer;
         }
     }
-
     return answer;
 }
