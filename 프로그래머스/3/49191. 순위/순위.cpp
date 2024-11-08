378cb0e3d0aa9fac32bd957bb69e2392d328f084
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-08 15:55:05 +0900

    [level 3] Title: 순위, Time: 1.36 ms, Memory: 4.26 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,54 +1,56 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
+#define NON_RESULT 0
 int solution(int n, vector<vector<int>> results) 
 {
-    vector<vector<int>> Graph(n + 1, vector<int>(n + 1, 0));
-    
-    for (vector<int> Res : results)
+    vector<vector<int>> Graph(n+1 , vector<int>(n + 1, NON_RESULT));
+    for(vector<int> result : results)
     {
-        int Winner = Res[0];
-        int Loser = Res[1];
-        Graph[Winner][Loser] = 1;
-        Graph[Loser][Winner] = -1;
+        Graph[result[0]][result[1]] = 1;
+        Graph[result[1]][result[0]] = -1;
     }
-
-
-    for (int k = 1; k <= n; ++k) 
+    
+    for(int k = 1; k <= n; ++k)
     {
-        for (int i = 1; i <= n; ++i) 
+        for(int i = 1; i <= n; ++i)
         {
-            for (int j = 1; j <= n; ++j) 
+            if(i == k)
             {
-                if (Graph[i][k] == 1 && Graph[k][j] == 1) 
+                continue;
+            }
+            
+            for(int j = 1; j <= n; ++j)
+            {
+                if(Graph[i][k] == 1 && Graph[k][j] == 1)
                 {
                     Graph[i][j] = 1;
-                    Graph[j][i] = -1; // j는 i에게 진다
+                    Graph[j][i] = -1;
                 }
             }
         }
     }
-
+    
     int answer = 0;
-    for (int i = 1; i <= n; ++i)
+    for(int i = 1; i <= n; ++i)
     {
-        int UnableToRank = 0;
-        for (int j = 1; j <= n; ++j)
+        int ResultCount = 0;
+        for(int j = 1; j <= n; ++j)
         {
-            if (Graph[i][j] != 0)
+            if(Graph[i][j] != 0)
             {
-                ++UnableToRank;
+                
+                ++ResultCount;
             }
         }
-
-        if (UnableToRank == n - 1)
+        
+        if(n - 1 == ResultCount)
         {
             ++answer;
         }
     }
-
+    
+    
     return answer;
 }
-
