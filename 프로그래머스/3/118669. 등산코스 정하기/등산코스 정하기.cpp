34301223615c4757bb10ba14ea4dcf37e872ad31
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-11 13:51:15 +0900

    [level 3] Title: 등산코스 정하기, Time: 57.62 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -1,99 +1,83 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-#define INDEX_MAX 999999
-#define COST_MAX 99999999
-vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits)
+#define INF 99999999
+
+vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) 
 {
-    vector<bool> IsGates(n + 1, false);
-    vector<bool> IsSummit(n + 1, false);
-    
-    for(int SummitNode : summits)
-    {
-        IsSummit[SummitNode] = true;
-    }
-    
     vector<vector<pair<int, int>>> Graph(n + 1);
-    for (vector<int> Path : paths)
+    
+    for(vector<int> Path : paths)
     {
         int From = Path[0];
         int To = Path[1];
-        int Rset = Path[2];
+        int Rest = Path[2];
         
-        Graph[From].push_back({ Rset, To });
-        Graph[To].push_back({ Rset, From });
+        Graph[From].push_back({Rest, To});
+        Graph[To].push_back({Rest, From});
     }
     
-    //         Rset  From
-    vector<pair<int, int>> MinRestToNode(n + 1, { COST_MAX, INDEX_MAX });
-
-    //                  Rset    From,  To
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> PQ;
-
-    for (int GateNode : gates)
+    vector<int> MinRestToNode(n + 1, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ;
+    
+    vector<bool> GateCheck(n + 1, false);
+    for(int GateNode : gates)
     {
-        IsGates[GateNode] = true;
-        for (pair<int, int> NextNode : Graph[GateNode])
-        {
-            PQ.push({ NextNode.first, {GateNode, NextNode.second} });
-        }
+        GateCheck[GateNode] = true;
+        PQ.push({0, GateNode});
     }
-
-    while (!PQ.empty())
+    
+    vector<bool> SummitCheck(n + 1, false);
+    for(int SummitNode : summits)
     {
-        int CurMaxRest = PQ.top().first;
-        int FromNode = PQ.top().second.first;
-        int CurNode = PQ.top().second.second;
+        SummitCheck[SummitNode] = true;
+    }
+    
+    while(!PQ.empty())
+    {
+        int CurNode = PQ.top().second;
+        int CurRest = PQ.top().first;
         PQ.pop();
         
-        if (IsSummit[CurNode] == true)
+        if(SummitCheck[CurNode] == true)
         {
-            if (MinRestToNode[CurNode].first > CurMaxRest)
-            {
-                MinRestToNode[CurNode].first = CurMaxRest;
-                MinRestToNode[CurNode].second = FromNode;
-            }
             continue;
         }
-
-        for (pair<int, int> Path : Graph[CurNode])
+        
+        if (MinRestToNode[CurNode] < CurRest)
         {
-            int MaxRest = max(Path.first, CurMaxRest);
-            int NextNode = Path.second;
-            
-            if (IsGates[NextNode] == true)
+            continue;
+        }
+        
+        for(pair<int, int> Next : Graph[CurNode])
+        {
+            int NextNode = Next.second;
+            if(GateCheck[NextNode] == true)
             {
                 continue;
             }
             
-            if (MinRestToNode[NextNode].first > MaxRest)
+            int NewRest = max(CurRest, Next.first);
+            if(MinRestToNode[NextNode] > NewRest)
             {
-                MinRestToNode[NextNode].first = MaxRest;
-                MinRestToNode[NextNode].second = FromNode;
-                PQ.push({ MaxRest, {FromNode, NextNode} });
-            }
-            else if (MinRestToNode[NextNode].first == MaxRest)
-            {
-                if(MinRestToNode[NextNode].second > FromNode)
-                {
-                    MinRestToNode[NextNode].second = FromNode;
-                    PQ.push({ MaxRest, {FromNode, NextNode} });
-                }
+                MinRestToNode[NextNode] = NewRest;
+                PQ.push({NewRest, NextNode});
             }
         }
     }
     
-    vector<int> answer = { INDEX_MAX, COST_MAX };
-    for (int Summit : summits)
+    vector<int> answer = {INF, INF};
+    for(int Summit : summits)
     {
-        if (answer[1] > MinRestToNode[Summit].first)
+        if(MinRestToNode[Summit] < answer[1])
         {
-            answer[1] = MinRestToNode[Summit].first;
+            answer[1] = MinRestToNode[Summit];
             answer[0] = Summit;
         }
-        else if (answer[1] == MinRestToNode[Summit].first)
+        else if(MinRestToNode[Summit] == answer[1])
         {
             answer[0] = min(answer[0], Summit);
         }
