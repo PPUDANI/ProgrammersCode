b709fc4866ec988f546908e2eb8d75bc37f7ab6f
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-12 14:10:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+#define NO_DATA -1
+
+int Fibonachi(int n, vector<int>& DP)
+{
+    if(DP[n] != NO_DATA)
+    {
+        return DP[n];
+    }
+    
+    DP[n] = Fibonachi(n - 1, DP) + Fibonachi(n - 2, DP);
+    return DP[n] % 1234567;
+}
+
+long long solution(int n)
+{
+    vector<int> DP(n + 1, NO_DATA);
+    DP[0] = 1;
+    DP[1] = 1;
+    int answer = Fibonachi(n, DP);
+    return answer;
+}
+
