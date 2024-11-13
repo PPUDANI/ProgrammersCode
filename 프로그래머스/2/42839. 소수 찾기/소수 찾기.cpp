6535c4ad90a15b2f30999af16110447b98e7fd38
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-13 17:50:33 +0900

    [level 2] Title: 소수 찾기, Time: 0.59 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -11,7 +11,6 @@ bool IsPrime(int Num)
     if(Num == 2) return true;
     if(Num % 2 == 0) return false;
     
-    // 3부터 시작하여 홀수만 검사
     for(int i = 3; i * i <= Num; i += 2)
     {
         if(Num % i == 0)
@@ -25,11 +24,8 @@ bool IsPrime(int Num)
 int solution(string numbers) 
 {
     set<int> Permutations;
-
-    // 숫자들을 오름차순으로 정렬
     sort(numbers.begin(), numbers.end());
 
-    // 순열을 만들고, Permutations에 넣기
     do {
         for (int i = 1; i <= numbers.length(); ++i) {
             int num = stoi(numbers.substr(0, i));
@@ -39,7 +35,6 @@ int solution(string numbers)
     
     int answer = 0;
     
-    // Permutations에서 소수 판별 후 카운트
     for (int Permutation : Permutations)
     {
         if (IsPrime(Permutation))
