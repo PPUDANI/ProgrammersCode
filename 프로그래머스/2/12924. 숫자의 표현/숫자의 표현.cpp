12161f28d894fba6d1f862141da3e1e72b379b0b
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-15 15:50:58 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) 
+{
+    int answer = 0;
+    
+    for(int i = 1; i <= n; i += 2)
+    {
+        if(n % i == 0 && i % 2 == 1)
+        {
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
