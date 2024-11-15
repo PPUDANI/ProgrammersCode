963015750106156039ed5971cdb5a867d6dcfcd6
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-15 16:01:50 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -4,15 +4,26 @@ using namespace std;
 
 int solution(int n) 
 {
-    int answer = 0;
-    
-    for(int i = 1; i <= n; i += 2)
+    if(n == 1)
+    {
+        return 1;
+    }
+    int answer = 1; // 본인
+
+    int loop = n;
+    if(n % 2 == 1) // 홀수는 무조건 하나 가능
+    {
+        ++answer;
+    }
+     
+    for(int i = 3; i < loop; i += 2)
     {
         if(n % i == 0 && i % 2 == 1)
         {
             ++answer;
         }
     }
+    
     
     return answer;
 }
