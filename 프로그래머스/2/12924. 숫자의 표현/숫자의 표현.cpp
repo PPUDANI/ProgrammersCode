af96b395831e2e44bf9092c840a613d978f31d3e
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-15 17:28:57 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -1,24 +1,21 @@
 #include <string>
 #include <vector>
-#include <cmath>
-#include <iostream>
+
 using namespace std;
 
-int solution(int n) 
-{
-    if(n <= 2)
-    {
-        return 1;
-    }
+int solution(int n) {
+    int answer = 0;
     
-    int answer = 1;
-    for (int i = 3; i <= n; i += 2)
-	{
-		if (n % i == 0)
-		{
-			answer += 1;
-		}
-	}
+    for (int start = 1; start <= n; start++) {
+        int sum = 0;
+        for (int num = start; sum < n; num++) {
+            sum += num;
+            if (sum == n) {
+                answer++;
+                break;
+            }
+        }
+    }
     
     return answer;
 }
