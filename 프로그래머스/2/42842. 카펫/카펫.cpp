16dc98358020669d32a3fc42167ebff97b07cd1c
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-19 13:08:20 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,14 +5,24 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) 
 {
+    // All - Yellow = Brown;
+    // Ax * Ay - Yx * Yy = Brown;
+    // Ax * Ay - (Ax - 2) * (Ay - 2) = Brown;
+    // Ax * Ay - (Ax * Ay - 2Ax - 2Ay + 4) = Brown;
+    // Ax * Ay - Ax * Ay + 2Ax + 2Ay - 4 = Brown;
+    // 2Ax + 2Ay - 4 = Brown;
+    // 
+    // Ax * Ay = All
+    // Ax + Ay = (Brown / 2) + 2;
+
     vector<int> answer(2, 0);
     int WidthPlusHeight = (brown / 2) + 2;
-    int NumOfTile = brown + yellow;
+    int AllTile = brown + yellow;
     
     for(int i = 1; i < WidthPlusHeight; ++i)
     {
-        int CurPair = (WidthPlusHeight - i) * i;
-        if(NumOfTile == CurPair)
+        int CheckTile = (WidthPlusHeight - i) * i;
+        if(AllTile == CheckTile)
         {
             answer[0] = WidthPlusHeight - i;
             answer[1] = i;
