2bbb77a7da332a021b9e56e0ede5f4b6a89d4bc1
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-19 11:19:28 +0900

    [level 3] Title: 합승 택시 요금, Time: 10.82 ms, Memory: 5.51 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,41 +1,43 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-#define INF 9999999
+#define INF 99999999
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) 
 {
-    vector<vector<int>> Graph(n+1, vector<int>(n+1, INF));
-    
+    vector<vector<int>> Graph(n + 1, vector<int>(n + 1, INF));
+    for(vector<int> FaresInfo : fares)
+    {
+        Graph[FaresInfo[0]][FaresInfo[1]] = FaresInfo[2];
+        Graph[FaresInfo[1]][FaresInfo[0]] = FaresInfo[2];
+    }
     for(int i = 1; i <= n; ++i)
     {
         Graph[i][i] = 0;
     }
     
-    for(vector<int> Edge : fares)
-    {
-        Graph[Edge[0]][Edge[1]] = Edge[2];
-        Graph[Edge[1]][Edge[0]] = Edge[2];
-    }
-    
+    // Floid-Washal
     for(int k = 1; k <= n; ++k)
     {
         for(int i = 1; i <= n; ++i)
         {
-            if(i == k)
+            if(k == i)
             {
                 continue;
             }
-            
-            for(int j = 1; j <= n; ++j)
+            for(int j = 1; j  <= n; ++j)
             {
-                int SumFares = Graph[i][k] + Graph[k][j];
-                if(SumFares < INF && Graph[i][j] > SumFares)
+                if(k == j)
                 {
-                    Graph[i][j] = SumFares;
-                    Graph[j][i] = SumFares;
+                    continue;
+                }
+                
+                int NewFares = Graph[i][k] + Graph[k][j];
+                if(NewFares < Graph[i][j])
+                {
+                    Graph[i][j] = NewFares;
+                    Graph[j][i] = NewFares;
                 }
             }
         }
@@ -44,9 +46,11 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares)
     int answer = INF;
     for(int i = 1; i <= n; ++i)
     {
-        int Curfares = Graph[s][i] + Graph[i][a]+ Graph[i][b];
-        answer = min(answer, Curfares);
+        int CurFares = Graph[s][i] + Graph[i][a] + Graph[i][b];
+        answer = min(answer, CurFares);
     }
+    
+    
     
     return answer;
 }
