4b98a4bde244045ddffcbb36d3eb26ea3c95ad3a
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-21 15:22:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,21 +1,13 @@
-#include <iostream>
-
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
     int Div = 10;
-    while(true)
+    while(n != 0)
     {
         int CurNum = n % Div;
         n /= Div;
-        cout << CurNum << endl;
         answer += CurNum;
-        if(n == 0)
-        {
-            break;
-        }
     }
-
     return answer;
 }
