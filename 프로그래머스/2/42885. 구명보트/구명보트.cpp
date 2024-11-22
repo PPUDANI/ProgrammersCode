54946502b8521c3c75d5fc4cb36590bc6b9047d6
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-11-22 14:31:21 +0900

    [level 2] Title: 구명보트, Time: 1.88 ms, Memory: 4.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int solution(vector<int> people, int limit)
+{
+    vector<int> ASC_People(people.begin(), people.begin());
+    sort(people.begin(), people.end(), [](int Left, int Right)
+         { 
+             return Left > Right; 
+         });
+    
+    int DESC_index = 0;
+    int ASC_index = int(people.size()) - 1;
+    
+    int answer = 0;
+    while(true)
+    {
+        if(people[DESC_index] + people[ASC_index] <= limit)
+        {
+            --ASC_index;
+            ++DESC_index;
+        }
+        else
+        {
+            ++DESC_index;
+        }
+        ++answer;
+        
+        if(DESC_index == ASC_index)
+        {
+            ++answer;
+            break;
+        }
+        
+        if(DESC_index > ASC_index)
+        {
+            break;
+        }
+    }
+    
+    return answer;
+}
