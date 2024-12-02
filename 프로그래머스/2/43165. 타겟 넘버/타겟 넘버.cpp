ef9bbe82288f4e5ccb7b6ce15a73979d751e160d
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-12-02 14:10:54 +0900

    [level 2] Title: 타겟 넘버, Time: 4.80 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,36 +1,35 @@
+#include <string>
 #include <vector>
 #include <stack>
 using namespace std;
 
-
-int solution(vector<int> numbers, int target) {
+int solution(vector<int> numbers, int target) 
+{
     int answer = 0;
+    int NumOfNum = int(numbers.size());
     
     stack<pair<int, int>, vector<pair<int, int>>> DPS;
     
     DPS.push({-numbers[0], 0});
     DPS.push({numbers[0], 0});
     
-    int NumSize = int(numbers.size());
     while(!DPS.empty())
     {
         int CurAccum = DPS.top().first;
-        int CurIndex = DPS.top().second;
+        int NextIndex = DPS.top().second + 1;
         DPS.pop();
-        
-        int NextIndex = CurIndex + 1;
-        if(NextIndex == NumSize)
+        if(NextIndex < NumOfNum)
+        {
+            DPS.push({CurAccum - numbers[NextIndex], NextIndex});
+            DPS.push({CurAccum + numbers[NextIndex], NextIndex});
+        }
+        else
         {
             if(CurAccum == target)
             {
                 ++answer;
             }
-            continue;
         }
-        
-        int NextAccum = numbers[NextIndex];
-        DPS.push({CurAccum - NextAccum, NextIndex});
-        DPS.push({CurAccum + NextAccum, NextIndex});
     }
     
     return answer;
