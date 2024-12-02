9f7e86b1f1a73b3dd59c6ff7b37471b70ba1c761
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2024-12-02 15:49:37 +0900

    [level 2] Title: 더 맵게, Time: 204.27 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) 
+{
+    int answer = 0;
+    
+    priority_queue<int, vector<int>, greater<int>> PQ;
+    
+    for(int Val : scoville)
+    {
+        PQ.push(Val);
+    }
+    
+    while(PQ.top() < K)
+    {
+        if(PQ.size() < 2)
+        {
+            return -1;
+        }
+        
+        int CurMin_First = PQ.top();
+        PQ.pop();
+        
+        int CurMin_Second = PQ.top();
+        PQ.pop();
+        
+        PQ.push(CurMin_First + (2 * CurMin_Second));
+        ++answer;
+    }
+    
+    return answer;
+}
