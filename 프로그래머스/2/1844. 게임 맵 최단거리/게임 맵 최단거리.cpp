fbf9e331e986522846b2e12a32e345b60d35e397
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2025-01-07 11:03:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.31 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -8,52 +8,65 @@ struct int2
     int x;
     int y;
     
-    int2 operator +(int2 _Val)
+    int2 operator+(int2 _Val)
     {
         int2 Res;
-        Res.x = _Val.x + x;
-        Res.y = _Val.y + y;
+        Res.x = x + _Val.x;
+        Res.y = y + _Val.y;
         return Res;
     }
+    
+    bool operator ==(int2 _Val)
+    {
+        return x == _Val.x && y == _Val.y;
+    }
+    
 };
 
 int solution(vector<vector<int> > maps)
 {
-    int answer = 0;
-    int XSize = int(maps[0].size());
-    int YSize = int(maps.size());
+    int Max_x = int(maps[0].size());
+    int Max_y = int(maps.size());
+    int2 Goal = {Max_x - 1, Max_y - 1};
     
     queue<pair<int2, int>> BFS;
-    vector<vector<bool>> Visit(YSize, vector<bool>(XSize, false));
+    vector<vector<bool>> Visit(Max_y, vector<bool>(Max_x, false));
     
-    //                            Up      Down    Right   Left
-    vector<int2> Dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-
     BFS.push({{0, 0}, 1});
-    maps[0][0] = 0;
-    
+    Visit[0][0] = true;
+
+    vector<int2> MoveDirs = {{-1, 0},{1, 0},{0, -1},{0, 1}};
     while(!BFS.empty())
     {
-        int2 CurLocation = BFS.front().first;
-        int NumOfMovedTile = BFS.front().second;
+        int2 CurTile = BFS.front().first;
+        int CurMoveCount = BFS.front().second;
         BFS.pop();
         
-        if(CurLocation.y == YSize - 1 && CurLocation.x == XSize - 1 )
+        if(CurTile == Goal)
         {
-            return NumOfMovedTile;
+            return CurMoveCount;
         }
         
-        for(int2 Dir : Dirs)
+        for(int2 MoveDir : MoveDirs)
         {
-            int2 NextLocation = CurLocation + Dir;
-            if((NextLocation.y >= 0 && NextLocation.y < YSize && NextLocation.x >= 0 && 
-               NextLocation.x < XSize) && maps[NextLocation.y][NextLocation.x] == 1)
-            {    
-                maps[NextLocation.y][NextLocation.x] = 0;
-                BFS.push({NextLocation, NumOfMovedTile + 1});
+            int2 NextTile = CurTile + MoveDir;
+            if(NextTile.x < 0 || NextTile.y < 0 || NextTile.x >= Max_x || NextTile.y >= Max_y)
+            {
+                continue;
+            }
+            
+            if(maps[NextTile.y][NextTile.x] == 0)
+            {
+                continue;
+            }
+            
+            if(Visit[NextTile.y][NextTile.x] == false)
+            {
+                Visit[NextTile.y][NextTile.x] = true;
+                BFS.push({NextTile, CurMoveCount + 1});
             }
         }
     }
-
+    
     return -1;
 }
