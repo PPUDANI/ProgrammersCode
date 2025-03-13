314ab775ca24a1dbf3e6ef5246e8484d1570d129
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2025-03-13 15:21:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -6,7 +6,5 @@ using namespace std;
 string solution(vector<string> seoul)
 {
     int Index = find(seoul.begin(), seoul.end(), "Kim") - seoul.begin();
-    
-    string answer = "김서방은 " + to_string(Index) + "에 있다";
-    return answer;
+    return "김서방은 " + to_string(Index) + "에 있다";
 }
