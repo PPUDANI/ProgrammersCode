d290b03059a5959d5ba2ac58399f4e9ae23e145f
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2025-03-13 14:37:42 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -2,23 +2,32 @@
 #include <vector>
 #include <iostream>
 #include <set>
+#include <algorithm>
 using namespace std;
 
 long long solution(long long n) 
 {
-    std::multiset<int, greater<int>> Set;
-    while(n > 0)
-    {
-        Set.insert(n % 10);
-        n /= 10;
-    }
+    //std::multiset<int, greater<int>> Set;
+    //while(n > 0)
+    //{
+    //    Set.insert(n % 10);
+    //    n /= 10;
+    //}
+    //
+    //long long answer = 0;
+    //for(int i : Set)
+    //{
+    //    answer *= 10;
+    //    answer += i;
+    //}
+//
+    //return answer;
     
     long long answer = 0;
-    for(int i : Set)
-    {
-        answer *= 10;
-        answer += i;
-    }
+
+    string str = to_string(n);
+    sort(str.begin(), str.end(), greater<char>());
+    answer = stoll(str);
 
     return answer;
 }
