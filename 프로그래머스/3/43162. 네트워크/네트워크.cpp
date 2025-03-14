bba5eea2a818bab3b8563e5e503120060e5781c6
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2025-03-14 15:35:48 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,51 +3,46 @@
 #include <stack>
 using namespace std;
 
-
-
-
-int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
+int solution(int n, vector<vector<int>> computers) 
+{
+    stack<int, vector<int>> DFS;
     
     vector<bool> Visit(n, false);
-    stack<int, vector<int>> DFS;
+    
     DFS.push(0);
     Visit[0] = true;
     
+    int VisitMax = 0;
     bool IsEnd = false;
+    int answer = 0;
     while(IsEnd == false)
     {
         while(!DFS.empty())
         {
-            int CurNum = DFS.top();
+            int CurIndex = DFS.top();
+            Visit[CurIndex] = true;
             DFS.pop();
-            
+
             for(int i = 0; i < n; ++i)
             {
-                if(computers[CurNum][i] == 1 && Visit[i] == false)
+                if(computers[CurIndex][i] == 1 && Visit[i] == false)
                 {
-                    Visit[i] = true;
                     DFS.push(i);
                 }
             }
         }
-        ++answer;
         
-        bool EndCheck = true;
-        for(int i = 0; i < n; ++i)
+        ++answer;
+        IsEnd = true;
+        for(int i = VisitMax + 1; i < n; ++i)
         {
             if(Visit[i] == false)
             {
-                EndCheck = false;
-                Visit[i] = true;
+                IsEnd = false;
                 DFS.push(i);
+                VisitMax = i;
                 break;
             }
-        }
-        
-        if(EndCheck == true)
-        {
-            IsEnd = true;
         }
     }
     
