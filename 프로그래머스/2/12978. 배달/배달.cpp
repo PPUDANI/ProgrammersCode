b62fb3397bd5552a758911f69fa56d8c3c08a9a0
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2025-03-15 20:29:45 +0900

    [level 2] Title: 배달, Time: 0.33 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 #define INF 2147483647
 
-int solution(int N, vector<vector<int> > road, int K) 
+int solution(int N, vector<vector<int>> road, int K) 
 {
-    int answer = 0;
-    //         Cost  NextNode
     vector<vector<pair<int, int>>> Graph(N + 1, vector<pair<int, int>>());
-    for(vector<int> EdgeInfo : road)
+    
+    for(vector<int> Edge : road)
     {
-        Graph[EdgeInfo[0]].push_back({EdgeInfo[2], EdgeInfo[1]});  
-        Graph[EdgeInfo[1]].push_back({EdgeInfo[2], EdgeInfo[0]});
+        Graph[Edge[0]].push_back({Edge[1], Edge[2]});
+        Graph[Edge[1]].push_back({Edge[0], Edge[2]});
     }
     
-    priority_queue<pair<int, int>> PQ;
-    PQ.push({0, 1});
-    
     vector<int> CostToNode(N + 1, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>> PQ;
+    
     CostToNode[1] = 0;
+    PQ.push({1, 0});
     
     while(!PQ.empty())
     {
-        int CurCost = PQ.top().first;
-        int CurNode = PQ.top().second;
+        int CurNode = PQ.top().first;
+        int CurCost = PQ.top().second;
         PQ.pop();
         
         if(CurCost > CostToNode[CurNode])
@@ -35,24 +35,25 @@ int solution(int N, vector<vector<int> > road, int K)
         
         for(pair<int, int> Edge : Graph[CurNode])
         {
-            int NewCost = CurCost + Edge.first;
-            int NextNode = Edge.second;
-            
+            int NextNode = Edge.first;
+            int NewCost = CurCost + Edge.second;
             if(NewCost < CostToNode[NextNode])
             {
                 CostToNode[NextNode] = NewCost;
-                PQ.push({NewCost, NextNode});
+                PQ.push({NextNode, NewCost});
             }
         }
-    }
+    }       
     
-    for(int i = 1; i <= N; ++i)
+    int answer = 0;
+    for(int i : CostToNode)
     {
-        if(CostToNode[i] <= K)
+        if(i <= K)
         {
             ++answer;
         }
     }
     
-    return  answer;
+
+    return answer;
 }
